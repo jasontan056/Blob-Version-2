52953ff8f2d213e42ce316b5765c6d94fbd0b4f6
jasontan056 <jasontan056@gmail.com>
2020-03-26 13:23:49 -0700

    Add seperation force

--- vertex.js
+++ vertex.js
@@ -1,15 +1,16 @@
-const FRICTION_CONSTANT = 0.3;
-const MAX_SPEED = 100;
+const FRICTION_CONSTANT = 0.1;
+const MAX_SPEED = 10;
+const MAX_FORCE = 0.5;
 
 class Vertex {
   pos;
+  blobId;
 
-  #blobId;
   #vel;
   #acc = p5.Vector.random2D().mult(0);
 
   constructor(blobId, startingPosition, startingVelocity) {
-    this.#blobId = blobId;
+    this.blobId = blobId;
     this.pos = startingPosition;
     this.#vel = startingVelocity;
   }
@@ -40,7 +41,27 @@ class Vertex {
 
   // Calculates separation force from nearby vertexes.
   #calcSeparationForce = nearbyVertexes => {
-    // !!! implement
-    return 0;
+    const totalSteer = createVector(0, 0);
+    let count = 0;
+    for (vertex of nearbyVertexes) {
+      const diff = p5.Vector.sub(this.pos, vertex.pos);
+      const distance = diff.mag();
+      const steer = diff.normalize().div(distance);
+      totalSteer.add(steer);
+      count++;
+    }
+
+    if (count > 0) {
+      totalSteer.div(count);
+    }
+
+    if (totalSteer.mag() > 0) {
+      // Implement Reynolds: Steering = Desired - Velocity
+      totalSteer.normalize();
+      totalSteer.mult(MAX_SPEED);
+      totalSteer.sub(this.velocity);
+      totalSteer.limit(MAX_FORCE);
+    }
+    return totalSteer;
   };
 }
